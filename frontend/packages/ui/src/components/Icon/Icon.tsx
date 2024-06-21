1d7f43f1f635d0b9a0a6e0d718d7e7480bf0ab9d
CreatiCoding <nodejsdeveloper@kakao.com>
2024-06-21 22:44:58 +0900

    feat(ui): Button / Input 추가 및 Storybook 추가

---
+++ frontend/packages/ui/src/components/Icon/Icon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SearchIcon } from './resources/SearchIcon';
+
+type IconName = 'search';
+
+export interface IconProps extends React.SVGAttributes<SVGAElement> {
+  name: IconName;
+}
+
+export function Icon({ name, ...props }: IconProps) {
+  switch (name) {
+    case 'search': {
+      return <SearchIcon {...props} />;
+    }
+  }
+}
+
+Icon.displayName = "Icon"
---
+++ frontend/zicdding-class.com/app/test/button/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from '@zicdding-web/ui/Button';
+
+export default function TestPage() {
+  return (
+    <>
+      <h1>테스트 페이지</h1>
+      <div>
+        <Button>클릭</Button>
+      </div>
+    </>
+  );
+}
