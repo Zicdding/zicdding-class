22ef0f7dd99a1ffe237df3a5b4b5877e91411e0f
CreatiCoding <nodejsdeveloper@kakao.com>
2024-06-23 00:36:03 +0900

    update branch

--- frontend/packages/ui/vite.config.ts
+++ frontend/packages/ui/vite.config.ts
@@ -4,6 +4,7 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
   base: '/zicdding-ui-storybook',
+  // NOTE: 너무 많은 파일을 업로드하면 github pages 가 특정 리소스를 404로 내려주는 이슈가 있어서 하나의 파일로 번들
   plugins: [viteSingleFile()],
   resolve: {
     alias: [
