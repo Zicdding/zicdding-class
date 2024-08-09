a2603062661e6e7a73b02d1867936aad781bb1bc
CreatiCoding <nodejsdeveloper@kakao.com>
2024-08-10 02:57:06 +0900

    tanstack query 로 변경

--- frontend/zicdding-class.com/app/_hooks/useUser.ts
+++ frontend/zicdding-class.com/app/_hooks/useUser.ts
@@ -1,49 +1,22 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
-import { apiV1 } from "../_remotes";
+import { useCallback } from "react";
+import { apiV1 } from "@/app/_remotes";
+import { useQuery } from "@tanstack/react-query";
 
 export function useUser() {
-  const [user, setUser] = useState<{
-    email: string;
-    nickname: string;
-    // FIXME: phone_num -> phoneNum
-    phone_num: string | null;
-  } | null>();
-
-  useEffect(() => {
-    (async () => {
-      try {
-        await apiV1.users.getMe().then(setUser);
-      } catch (error: any) {
-        setUser(null);
-
-        if (error.name === "HTTPError") {
-          console.error(await error.response.text());
-          // console.error(await error.response.json());
-        } else {
-          console.error(error.message);
-        }
-      }
-    })();
-  }, []);
+  const { data, refetch, isError } = useQuery({
+    initialData: undefined,
+    queryKey: ["useUser"],
+    queryFn: () => apiV1.users.getMe(),
+    retry: false,
+  });
 
   const logout = useCallback(async () => {
     await apiV1.users.logout();
 
-    setUser(null);
+    refetch();
+  }, [refetch]);
 
-    window.location.reload();
-  }, []);
-
-  return [
-    user
-      ? {
-          email: user.email,
-          nickname: user.nickname,
-          phoneNumber: user.phone_num,
-        }
-      : user,
-    logout,
-  ] as const;
+  return [isError ? null : data, logout] as const;
 }
