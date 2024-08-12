27807bcc40b45502e464b80a86fa395d9e0235b4
Jun Seong <drawbeat0112@gmail.com>
2024-08-12 17:24:24 +0900

    import 구문 수정

--- frontend/zicdding-class.com/app/components/ITNews.tsx
+++ frontend/zicdding-class.com/app/components/ITNews.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '@ui/components/Icon';
-import { ITCard, ITCardContent, ITCardDescription, ITCardFooter, ITCardHeader, ITCardTitle } from '@zicdding-web/ui';
+import * as IT from '@zicdding-web/ui';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -20,22 +20,22 @@ interface Props {
 
 export default function ITNews({ id, title, imgUrl, type, date, likeCnt, viewCnt, commentCnt, myLike }: Props) {
   return (
-    <ITCard className="relative w-[400px]">
-      <ITCardHeader className="relative w-full">
+    <IT.ITCard className="relative w-[400px]">
+      <IT.ITCardHeader className="relative w-full">
         <Image src={imgUrl} alt={title} layout="fill" />
-      </ITCardHeader>
+      </IT.ITCardHeader>
       <Link href={`/news/${id}`}>
-        <ITCardContent>
-          <ITCardDescription className="mt-[10px] leading-4 px-2 py-1 inline-block">{type}</ITCardDescription>
-          <ITCardTitle className="mt-[10px]">{title}</ITCardTitle>
+        <IT.ITCardContent>
+          <IT.ITCardDescription className="mt-[10px] leading-4 px-2 py-1 inline-block">{type}</IT.ITCardDescription>
+          <IT.ITCardTitle className="mt-[10px]">{title}</IT.ITCardTitle>
           <div className="flex gap-1 mt-[10px]">
             <span>기간 :</span>
             <span className="font-semibold">
               {date.start} ~ {date.end}
             </span>
           </div>
-        </ITCardContent>
-        <ITCardFooter className="mt-5">
+        </IT.ITCardContent>
+        <IT.ITCardFooter className="mt-5">
           <ul className="flex items-center justify-end gap-4">
             <li className="flex gap-1">
               <span>⭐️</span>
@@ -50,11 +50,11 @@ export default function ITNews({ id, title, imgUrl, type, date, likeCnt, viewCnt
               <span>{commentCnt}</span>
             </li>
           </ul>
-        </ITCardFooter>
+        </IT.ITCardFooter>
       </Link>
       <div className="absolute cursor-pointer top-1 right-1">
         <Icon name="star" className={`${myLike ? 'text-yellow-400' : 'text-gray-400'} w-10 h-10`} />
       </div>
-    </ITCard>
+    </IT.ITCard>
   );
 }
