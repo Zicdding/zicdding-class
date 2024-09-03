bb230b5bec670d0bed1b2072af7934b7fc0a345d
(yerin) <(young01299@naver.com)>
2024-09-03 12:20:44 +0900

    feat : itnews 좋아요 이미 좋아요 누른 상태일 경우 추가

--- backend/src/services/itNews/itNewsLike.js
+++ backend/src/services/itNews/itNewsLike.js
@@ -14,13 +14,20 @@ const process = {
     newsLike: async (req, res) => {
         const userId = req.user.userId;
         const { newsId } = req.params;
-        console.log(newsId)
+        const checkSql = 'SELECT SQL_NO_CACHE * from TB_ITNEWS_LIKE WHERE user_id = ? and itnews_id = ?';
         const sql = 'INSERT INTO TB_ITNEWS_LIKE(user_id, itnews_id, created_date) VALUES(?,?,now())';
         const data = [userId, newsId];
         try {
-            const [result] = await promisePool.query(sql, data);
-            if (result.affectedRows) {
-                setResponseJson(res, 200, '좋아요 완료');
+            const [checkResult] = await promisePool.query(checkSql, [userId, newsId]);
+            console.log(checkResult[0])
+            if (checkResult.length > 0) { // 이미 좋아요를 누른 경우
+                setResponseJson(res, 409, '이미 좋아요를 누른 상태입니다.');
+                return;
+            } else {
+                const [result] = await promisePool.query(sql, data);
+                if (result.affectedRows) {
+                    setResponseJson(res, 200, '좋아요 완료');
+                }
             }
         } catch (err) {
             console.log(err)
@@ -32,12 +39,14 @@ const process = {
         const { newsId } = req.params;
         console.log(newsId);
 
-        const checkSql = 'SELCT * from TB_ITNEWS_LIKE WHERE user_id = ? and itnews_id = ?';
+        const checkSql = 'SELECT * from TB_ITNEWS_LIKE WHERE user_id = ? and itnews_id = ?';
+        console.log(checkSql)
         try {
             const checkResult = await promisePool.query(checkSql, [userId, newsId]);
-            if (checkResult.affectedRows) {
+            console.log(checkResult)
+            if (checkResult.length > 0) {
                 const delSql = 'DELETE FROM TB_ITNEWS_LIKE WHERE user_id = ? and itnews_id = ?';
-                const delResult = await promisePool.query(delSql, [userId, newsId]);
+                const [delResult] = await promisePool.query(delSql, [userId, newsId]);
                 if (delResult.affectedRows) {
                     setResponseJson(res, 200, '좋아요 취소');
                 }
