c52f77bb8a94b9d472765e93d97db9f3b41c2b9e
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2024-10-29 22:14:34 +0900

    feat: icon title a11y 적용 및 props 를 전달

--- frontend/packages/ui/src/components/Icon/resources/SearchIcon.tsx
+++ frontend/packages/ui/src/components/Icon/resources/SearchIcon.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-export function SearchIcon({ className }: { className?: string }) {
+export function SearchIcon({ className, ...props }: { className?: string }) {
   return (
     <svg
       className={className}
@@ -9,7 +7,9 @@ export function SearchIcon({ className }: { className?: string }) {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
+      <title>search</title>
       <path
         d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
         stroke="#828282"
