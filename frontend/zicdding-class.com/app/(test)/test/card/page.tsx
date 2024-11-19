30719269a770cd041107d39feaebbec5d68054b0
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2024-11-19 22:18:31 +0900

    fix: type 에러 수정

--- frontend/zicdding-class.com/app/(test)/test/card/page.tsx
+++ frontend/zicdding-class.com/app/(test)/test/card/page.tsx
@@ -1,24 +1,21 @@
-import ClassCard from "@/app/_components/ClassCard";
+import { ClassCard } from '@/src/features/class-card';
 
 export default function TestPage() {
-    return (
-        <ClassCard
-            id={1}
-            title="Example Class Title"
-            endDate="2024-07-01"
-            positions={[
-                "프론트엔드",
-                "백엔드"
-            ]}
-            nickname="직띵"
-            likeCnt={10}
-            viewCnt={20}
-            commentCnt={5}
-            technology={[
-                { name: 'react', imgUrl: '/next.svg' },
-                { name: 'js', imgUrl: '/vercel.svg' }
-            ]}
-            myLike={false}
-        />
-    );
+  return (
+    <ClassCard
+      id="1"
+      title="Example Class Title"
+      endDate="2024-07-01"
+      positions={['프론트엔드', '백엔드']}
+      nickname="직띵"
+      likeCnt={10}
+      viewCnt={20}
+      commentCnt={5}
+      technology={[
+        { name: 'react', imgUrl: '/next.svg' },
+        { name: 'js', imgUrl: '/vercel.svg' },
+      ]}
+      myLike={false}
+    />
+  );
 }
