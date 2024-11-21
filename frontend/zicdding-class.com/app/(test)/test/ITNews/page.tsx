1a77fa8d6fb0b084060db6f1d9b3e28a1893d406
Jun Seong <drawbeat0112@gmail.com>
2024-11-21 22:40:34 +0900

    refactor : test 페이지 props 수정

--- frontend/zicdding-class.com/app/(test)/test/ITNews/page.tsx
+++ frontend/zicdding-class.com/app/(test)/test/ITNews/page.tsx
@@ -4,17 +4,15 @@ import ITNews from '@/app/_components/ITNewsCard';
 export default function TestPage() {
   return (
     <ITNews
-      id={1}
+      id="7b205a4d-f17f-4b65-a463-38a270d1ab39"
       title="샘플 IT 뉴스 제목"
       thumbnailSrc="https://via.placeholder.com/300x200"
       type="공모전"
-      date={{
-        start: '2024-01-01',
-        end: '2024-12-31',
-      }}
-      likeCount={15}
-      viewCount={99}
-      commentCount={13}
+      startedDate="2023-04-01"
+      endDate="2023-04-30"
+      likeCnt={15}
+      viewCnt={99}
+      commentCnt={13}
       myLike={false}
     />
   );
