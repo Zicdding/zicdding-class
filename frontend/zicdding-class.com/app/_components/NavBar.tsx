6c94c4ddb1fbd8d9dae5e5c4dea81ca37aa45997
CreatiCoding <nodejsdeveloper@kakao.com>
2024-12-09 00:51:51 +0900

    self review

--- frontend/zicdding-class.com/app/_components/NavBar.tsx
+++ frontend/zicdding-class.com/app/_components/NavBar.tsx
@@ -11,7 +11,7 @@ export function NavBar() {
     <header className="w-full bg-white fixed top-0 left-0">
       <div className="max-w-[1280px] flex justify-between items-center h-20 px-6 m-auto">
         <div className="flex items-center">
-          <Link href={'/'}>
+          <Link href="/">
             <img src="/logo.png" alt="Logo" className="h-8 mr-2" />
           </Link>
         </div>
